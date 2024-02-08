1d650351d440541acbeb77de7f228c74090e699b
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-02-08 03:25:17 +0000

    webapps changes 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
 
   get "/dice", :controller => "dice", :action => "index"
+
+  resources "companies"
+  resources "contacts"
+
+  #get "/companies", :controller => "companies", :action => "index"
+  #resources is the equivalent to the text above
 end
